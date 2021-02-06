dc93832b09d48cc6afec5d3ad7614214d26569da
Joseph Ritzik <joe.ritzik@gmail.com>
2021-02-05 22:36:47 -0500

    Moved exercise component to its own file

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
-const Exercise = props => (
-  <tr>
-    <td>{props.exercise.username}</td>
-    <td>{props.exercise.description}</td>
-    <td>{props.exercise.duration}</td>
-    <td>{props.exercise.date.substring(0,10)}</td>
-    <td>
-      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
-    </td>
-  </tr>
-)
+import Exercise from './exercise.component';
 
 export default class ExercisesList extends Component {
   constructor(props) {
